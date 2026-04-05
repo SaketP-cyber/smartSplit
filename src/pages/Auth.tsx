f4610a2c76b34301daaff378d1653d5b46a2f344
SaketP-cyber <kumarpurwar1@gmail.com>
2026-04-05 11:48:28 +0530

    Change redirect url

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -61,7 +61,7 @@ export default function Auth() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: window.location.origin,
       },
     });
     if (error) throw error;
